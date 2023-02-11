12c2d90672472689cf7335ea4b8cd8a354e8eacf
Aneta Walicka <aneta4595@wp.pl>
2023-02-11 11:15:27 +0100

    form-control-age

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FormControlNameComponent } from './components/form-control-name/form-control-name.component';
+import { FormControlAgeComponent } from './components/form-control-age/form-control-age.component';
 import { FormControlNameComponentModule } from './components/form-control-name/form-control-name.component-module';
+import { FormControlAgeComponentModule } from './components/form-control-age/form-control-age.component-module';
 
 @NgModule({
   imports: [
     RouterModule.forRoot([
       { path: 'form-control-name', component: FormControlNameComponent },
+      { path: 'form-control-age', component: FormControlAgeComponent }
     ]),
     FormControlNameComponentModule,
+    FormControlAgeComponentModule
   ],
   exports: [RouterModule],
 })
-export class AppRoutingModule {}
+export class AppRoutingModule { }
---
+++ src/app/components/form-control-age/form-control-age.component.ts
@@ -0,0 +1,15 @@
+import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-control-age',
+  styleUrls: ['./form-control-age.component.scss'],
+  templateUrl: './form-control-age.component.html',
+  encapsulation: ViewEncapsulation.Emulated,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class FormControlAgeComponent {
+  readonly age: FormControl = new FormControl('', [Validators.min(18)]);
+
+
+}
